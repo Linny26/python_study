93b972b3f8a66505e100d8ea6f3bf29bfba29759
Linny26 <58085775+Linny26@users.noreply.github.com>
2023-07-23 19:10:51 +0200

    Create initials.py

---
+++ initials.py
@@ -0,0 +1,8 @@
+#when I was young my younger sister hit me on my nose with a frying pan
+print('EEEEEE           SSSSSS')
+print('E               SS')
+print('EEEEEE            SSS')
+print('E                   SS')
+print('EEEEEE         SSSSSSS')
+
+
